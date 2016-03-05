0743952d62105c241057690b083f72d4f21c1d98
Pius Ladenburger <info@pius-ladenburger.de>
2016-03-05 20:15:08 +0100

    Commented Session Class

--- advanced/class.session.php
+++ advanced/class.session.php
@@ -1,6 +1,15 @@
 <?php
+// Published under the MIT License, Copyright (c) 2015 Pius Ladenburger
+
 class Session {
 	private $db, $data, $name, $chat;
+	
+	/**
+	* Create a new Bot Session object to manage session storage with Telegram Bots
+	* @param $name The Name of the Bot, this will be the key for the values in the database
+	* @param $chat The Chat which session variables should be retrieved
+	* @param $db The Database object to work with
+	*/
 	public function __construct($name, $chat, $db) {
 		$this->db = $db;
 		$this->name = $name;
@@ -13,20 +22,39 @@ class Session {
 			$this->data[$k] = $d;
 		}
 	}
+	
+	/**
+	* Retrieve a value
+	* @param $key The index of the value
+	*/
 	public function get($key) {
 		return (isset($this->data[$key]))? $this->data[$key]['Content']:false;
 	}
+	
+	/**
+	* Set a new value at a given index. If the index doesn't exist a new row is created in the database.
+	* @param The index of the value
+	* @param The new value for this index
+	*/
 	public function set($key, $value) {
 		if (isset($this->data[$key])) {
 			return $this->update($this->data[$key]['ID'], $value);
 		}
 		return $this->insert($key, $value);
 	}
+	
+	/**
+	* Helper function to insert rows
+	*/
 	private function insert($key, $content) {
 		$array = (is_array($content))? 1:0;
 		$content = ($array === 1)? json_encode($content):content;
 		return $this->db->query('INSERT INTO `Session` (`Bot`, `Chat`, `Key`, `Content`, `Array`) VALUES ("' . $this->db->esc($this->name) . '", "' . $this->db->esc($this->chat) . '", "' . $this->db->esc($key) . '", "' . $this->db->esc($content) . '", "' . $array . '")');
 	}
+	
+	/**
+	* Helper function to update rows
+	*/
 	private function update($id, $content) {
 		$array = (is_array($content))? 1:0;
 		$content = ($array === 1)? json_encode($content):content;
